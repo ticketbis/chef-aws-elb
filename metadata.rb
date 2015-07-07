3d2a8f7723b9019149c234ddb0d08bb8c903607e
Alberto Tablado <alberto@aluki.org>
2015-07-08 00:31:25 +0200

    Documentation and license

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,13 @@
 name 'aws-elb'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'all_rights'
-description 'Installs/Configures aws-elb'
-long_description 'Installs/Configures aws-elb'
+maintainer 'Alberto Tablado'
+maintainer_email 'alberto.tablado@ticketbis.com'
+source_url 'https://github.com/ticketbis/chef-aws-elb'
+license 'Apache v2.0'
+description 'Manage ELBs'
+long_description IO.read(File.join(
+  File.dirname(__FILE__), 'README.md'
+  )
+)
 version '0.1.0'
 
 depends 'aws-base'
